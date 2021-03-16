8d0122cbd4901adc3456f8e1b3c955bad565284c
Wessel van Ree <wesselvanree@gmail.com>
2021-03-16 18:38:10 +0100

    Fixed gatsby-image issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: "/notes",
+  pathPrefix: `/notes`,
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
